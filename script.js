ae903e9c49940e445edefd217ac0a978e38334aa
tigergamernew12345-design <tigergamernew12345@gmail.com>
2026-04-01 16:31:28 +0530

    script.js

---
+++ script.js
@@ -0,0 +1,37 @@
+// TYPING ANIMATION
+const text = "I build Discord Bots, Websites & Roblox Systems.";
+let i = 0;
+
+function typing() {
+  if (i < text.length) {
+    document.getElementById("typing").innerHTML += text.charAt(i);
+    i++;
+    setTimeout(typing, 50);
+  }
+}
+
+typing();
+
+
+// MOBILE MENU
+const menuBtn = document.getElementById("menu-btn");
+const navLinks = document.getElementById("nav-links");
+
+menuBtn.addEventListener("click", () => {
+  navLinks.classList.toggle("active");
+});
+
+
+// SCROLL REVEAL
+const reveals = document.querySelectorAll(".reveal");
+
+window.addEventListener("scroll", () => {
+  reveals.forEach(el => {
+    const windowHeight = window.innerHeight;
+    const elementTop = el.getBoundingClientRect().top;
+
+    if (elementTop < windowHeight - 100) {
+      el.classList.add("show");
+    }
+  });
+});
